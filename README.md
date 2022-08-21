2af933be99095ea1915bb55b374e6ca3b59a7d0d
Elena Kuznetsova <olenakuznietsova@yandex.ru>
2022-08-21 13:11:05 +0300

    Add TestBooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # qa_python
+
+test_add_new_book_add_two_books()  
+Проверяем, что книги добавляются в коллектор  
+
+ 
+test_set_book_rating()  
+Проверяем, что рейтинг можно добавить и он от 1 до 10
+
+test_get_book_rating()  
+Проверяем, словарь не пустой и рейтинг указывается в int 
+
+test_get_books_with_specific_rating()  
+Проверяем, что метод возвращает непустой список
+
+test_get_books_rating()  
+Проверяем, что метод возвращает весь список книг
+
+test_add_book_in_favorites()  
+Проверяем, что метод добавляет книгу в список
+
+test_delete_book_from_favorites()  
+Проверяем, что метод удаляет книгу из списка по названию
+
+test_get_list_of_favorites_books()  
+Проверяем, что получен список книг, которые добавлены в favorites
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from main import BooksCollector
 
+
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
@@ -20,5 +21,105 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_set_book_rating_three_to_exciting_book(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем новую книгу и выставляем ей рейтинг 3
+        name = 'Гордость и предубеждение и зомби'
+        rating = 3
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+
+        assert collector.books_rating[name] in range(1, 11)
+
+    def test_get_book_rating(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем новую книгу и выставляем ей рейтинг 10
+        name = 'Война и мир и любовь'
+        rating = 10
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+
+        assert type(collector.get_book_rating(name)) == int
+        assert collector.get_book_rating(name)
+
+    def test_get_books_with_specific_rating_ten(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем новую книгу и выставляем ей рейтинг 100
+        name = 'Война и мир и любовь'
+        rating = 10
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+
+        assert len(collector.get_books_with_specific_rating(rating)) == 1
+
+    def test_get_books_rating_two_books(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем две книги и выставляем им рейтинг
+        name1 = 'Сказки Пушкина'
+        rating1 = 9
+        name2 = 'Русские народные сказки'
+
+        collector.add_new_book(name1)
+        collector.set_book_rating(name1, rating1)
+
+        collector.add_new_book(name2)
+
+        assert type(collector.get_books_rating()) == dict
+        assert len(collector.get_books_rating()) == 2
+
+    def test_add_book_in_favorites_one_book(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем книгу и выставляем ей рейтинг 10
+        name = 'Сказки Пушкина'
+        rating = 10
+
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+
+        collector.add_book_in_favorites(name)
+
+        assert len(collector.favorites) > 0
+        assert name in collector.favorites
+
+    def test_delete_book_from_favorites_only_book(self):
+        # создаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # добавляем книгу и выставляем ей рейтинг 10
+        name = 'Сказки Пушкина'
+        rating = 10
+
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+
+        # Добавлеяем книгу в favorites
+        collector.add_book_in_favorites(name)
+
+        collector.favorites.remove(name)
+
+        assert len(collector.favorites) == 0
+        assert name not in collector.favorites
+
+    def test_get_list_of_favorites_books(self):
+        # Cоздаем экземпляр (объект) класса BooksCollector
+        collector = BooksCollector()
+
+        # Добавляем книгу
+        name = 'Сказки Пушкина'
+        collector.add_new_book(name)
+
+        # Добавлеяем книгу в favorites
+        collector.add_book_in_favorites(name)
+
+        assert len(collector.get_list_of_favorites_books()) == 1
+        assert name in collector.get_list_of_favorites_books()
